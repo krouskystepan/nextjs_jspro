a365e1630617205eb5f1b892ca7cf39efdd7b5bf
Stepan Krousky <stepan.krousky@seznam.cz>
2023-09-19 21:15:35 +0200

    update

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Page = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full">
+        <div className='relative flex-center min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center'>
+          <h1 className='sm:heading-1 heading2 mb-6 text-center text-white'>JavaScript Mastery Resources</h1>
+        </div>
+      </section>
+    </main>
+  );
+};
 
-export default Page
+export default Page;
