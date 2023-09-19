98d75705bc54b431e947b87950c079d5e32e7325
Stepan Krousky <stepan.krousky@seznam.cz>
2023-09-19 23:35:27 +0200

    sanity done, res card

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,18 +1,48 @@
 import Filters from '@/components/Filters';
+import ResourceCard from '@/components/ResourceCard';
 import SearchForm from '@/components/SearchForm';
-import React from 'react';
+import { getResources } from '@/sanity/actions';
 
-const Page = () => {
+const Page = async () => {
+  const resources = await getResources({
+    query: '',
+    category: '',
+    page: '1',
+  });
+
+  console.log(resources);
+
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
-        <div className='relative flex-center min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center'>
-          <h1 className='sm:heading-1 heading2 mb-6 text-center text-white'>JavaScript Mastery Resources</h1>
+        <div className="relative flex-center min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center">
+          <h1 className="sm:heading-1 heading2 mb-6 text-center text-white">
+            JavaScript Mastery Resources
+          </h1>
         </div>
-        < SearchForm />
+        <SearchForm />
       </section>
 
-      < Filters />
+      <Filters />
+
+      <section className="flex-center mt-6 2-full flex-column sm:mt-20">
+        Header
+        <div className="mt-12 flex w-full flex-wrap justify-center ga[-16 sm:justify-start">
+          {resources?.length > 0 ? (
+            resources.map((resource: any) => (
+              <ResourceCard
+                key={resource._id}
+                title={resource.title}
+                id={resource._id}
+                image={resource.image}
+                downloadNumber={resource.views}
+              />
+            ))
+          ) : (
+            <p className="body-regular text-white-400">No resources find</p>
+          )}
+        </div>
+      </section>
     </main>
   );
 };
---
+++ components/ResourceCard.tsx
@@ -0,0 +1,53 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import Link from 'next/link';
+import Image from 'next/image';
+
+type ResourceCardProps = {
+  id: string;
+  title: string;
+  image: string;
+  downloadNumber: number;
+};
+
+const ResourceCard = ({
+  id,
+  title,
+  image,
+  downloadNumber,
+}: ResourceCardProps) => {
+  return (
+    <Card className="w-full max-w-fit border-0 !bg-transparent sm:max-w-[356px]">
+      <Link href={`/resource/${id}`}>
+        <CardHeader className="flex-center flex-col gap-2.5 !p-0">
+          <div className="h-fit w-full">
+            <Image
+              src={image}
+              alt={title}
+              className="h-full rounded-md object-center"
+              width={384}
+              height={440}
+            />
+          </div>
+          <CardTitle className="text-white paragraph-semibold line-clamp-1 w-full text-left">
+            {title}
+          </CardTitle>
+        </CardHeader>
+      </Link>
+      <CardContent className="flex-between mt-4 p-0">
+        <div className="flex-center body-medium gap-1.5 text-white">
+          <Image src="/downloads.svg" alt="downlaod" width={20} height={20} />
+          {downloadNumber}
+        </div>
+        <Link
+          href={`/resource/${id}`}
+          className="flex-center text-gradient_purple-blue body-semibold gap-1.5"
+        >
+          Download now{' '}
+          <Image src="/arrow-blue.svg" alt="" height={10} width={13} />
+        </Link>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ResourceCard;
